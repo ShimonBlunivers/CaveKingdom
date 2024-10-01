0ad5f9f6707f1f8cc3f9d0c06389c144cbb7306b
Blunivers <93725825+ShimonBlunivers@users.noreply.github.com>
2024-10-01 10:26:56 +0200

    Fixed

--- CaveKingdom/main.c
+++ CaveKingdom/main.c
@@ -9,10 +9,183 @@
 #include "inventory.h"
 #include "chunk.h"
 #include "camera.h"
-#include "renderer.h"
 
 
 int game_status = 1;
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+SDL_Texture* screen = NULL;
+SDL_Texture* gui = NULL;
+TTF_Font* font = NULL;
+SDL_Texture* text = NULL;
+
+
+typedef enum {
+    ui_element_inventory_slot,
+
+    number_of_ui_elements, // DO NOT USE AS UI ELEMENT!
+} UIElement;
+
+SDL_Texture* ui_textures[number_of_ui_elements] = { 0 };
+SDL_Texture* entity_textures[number_of_entity_types] = { 0 };
+SDL_Texture* item_textures[number_of_item_types] = { 0 };
+
+
+void load_textures() {
+    // UI textures
+    ui_textures[ui_element_inventory_slot] = IMG_LoadTexture(renderer, "./assets/textures/ui/inventory_slot.png");
+    printf("loaded texture %p", ui_textures[ui_element_inventory_slot]);
+
+    // Entity textures
+        // Ground
+    entity_textures[entity_type_water] = IMG_LoadTexture(renderer, "./assets/textures/tiles/water.png");
+    entity_textures[entity_type_dirt] = IMG_LoadTexture(renderer, "./assets/textures/tiles/dirt.png");
+    printf("%p\n", entity_textures[entity_type_water]);
+
+    // Surface
+    entity_textures[entity_type_player] = IMG_LoadTexture(renderer, "./assets/textures/tiles/player.png");
+    entity_textures[entity_type_enemy] = IMG_LoadTexture(renderer, "./assets/textures/tiles/enemy.png");
+    entity_textures[entity_type_zombie] = IMG_LoadTexture(renderer, "./assets/textures/tiles/zombie.png");
+    entity_textures[entity_type_stone] = IMG_LoadTexture(renderer, "./assets/textures/tiles/stone.png");
+    // Air
+
+
+// Item textures
+    item_textures[item_type_stone] = IMG_LoadTexture(renderer, "./assets/textures/items/stone.png");
+    item_textures[item_type_zombie_meat] = IMG_LoadTexture(renderer, "./assets/textures/items/zombie_meat.png");
+}
+
+void unload_textures() {
+    for (int i = 0; i < number_of_ui_elements; i++) {
+        SDL_DestroyTexture(ui_textures[i]);
+    }
+    for (int i = 0; i < number_of_entity_types; i++) {
+        SDL_DestroyTexture(entity_textures[i]);
+    }
+    for (int i = 0; i < number_of_item_types; i++) {
+        SDL_DestroyTexture(item_textures[i]);
+    }
+}
+void init_rendering() {
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE);
+
+    gui = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_SetTextureBlendMode(gui, SDL_BLENDMODE_BLEND);
+}
+
+
+void draw_world() {
+
+    SDL_SetRenderTarget(renderer, screen);
+    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+    SDL_RenderClear(renderer);
+
+    Entity entity;
+
+    SDL_Rect tile;
+    SDL_Color tile_color;
+
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            for (int layer = 0; layer < number_of_height_layers; layer++)
+            {
+                entity = *get_entity(x, y, layer);
+                if (!is_empty_entity_type(entity.type)) {
+                    tile = (SDL_Rect){ TILE_SIZE * x, TILE_SIZE * y, TILE_SIZE, TILE_SIZE };
+                    if (entity_textures[entity.type] != NULL) {
+                        SDL_RenderCopy(renderer, entity_textures[entity.type], NULL, &tile);
+                    }
+                    else {
+                        SDL_SetRenderDrawColor(renderer, 255 * ((float)y / MAP_HEIGHT), 0, 255 * ((float)x / MAP_WIDTH), 255);
+                        SDL_RenderFillRect(renderer, &tile);
+                    }
+                }
+            }
+        }
+    }
+
+
+    int max_width = TILE_SIZE;
+    int max_height = TILE_SIZE * 0.1;
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            entity = *get_entity(x, y, height_layer_surface);
+            if (entity.type != entity_type_surface_empty) {
+                if (entity.max_health > 0 && entity.max_health != entity.health) {
+                    int tile_x = entity.x * TILE_SIZE - max_width / 2 + TILE_SIZE / 2;
+                    int tile_y = entity.y * TILE_SIZE;
+                    SDL_Rect background_rect = { (tile_x - 1), (tile_y - 1), (max_width + 1), (max_height + 1) };
+                    SDL_Rect health_rect = { tile_x, tile_y, (max_width * ((float)entity.health / entity.max_health)), max_height };
+                    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                    SDL_RenderFillRect(renderer, &background_rect);
+                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                    SDL_RenderFillRect(renderer, &health_rect);
+                }
+            }
+        }
+    }
+
+    // UI
+
+    SDL_SetRenderTarget(renderer, gui);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+
+    Inventory inventory = main_player->inventory;
+
+    int inventory_width = SCREEN_WIDTH * .6;
+    int inventory_height = SCREEN_HEIGHT * .12;
+
+
+    SDL_Rect inventory_rect = { (SCREEN_WIDTH - inventory_width) / 2,  SCREEN_HEIGHT - inventory_height - 10, inventory_width, inventory_height };
+    //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    //SDL_RenderFillRect(renderer, &inventory_rect);
+
+    int slot_size = inventory_width / INVENTORY_SIZE;
+
+    int slot_index = 0;
+    int padding = 20;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = 0; i < INVENTORY_SIZE; i++) {
+        SDL_Rect slot_rect = { inventory_rect.x + slot_size * i, inventory_rect.y, slot_size, slot_size };
+        SDL_RenderCopy(renderer, ui_textures[ui_element_inventory_slot], NULL, &slot_rect);
+        if (inventory.content[i].type != item_type_empty) {
+            SDL_Rect item_rect = { inventory_rect.x + padding + slot_size * i * slot_index++, inventory_rect.y + padding, slot_size - padding * 2, slot_size - padding * 2 };
+
+            if (item_textures[inventory.content[i].type]) SDL_RenderCopy(renderer, item_textures[inventory.content[i].type], NULL, &item_rect);
+
+            char amount[128];
+            sprintf_s(amount, sizeof(amount), "%d", inventory.content[i].amount);
+
+            int digits = floor(log10((double)inventory.content[i].amount)) + 1;
+            int digit_size = 24;
+            SDL_Rect amount_rect = { slot_rect.x + padding / 2, slot_rect.h + slot_rect.y - padding / 2 - digit_size * 1.5, digit_size * digits , digit_size * 1.5 };
+
+            SDL_Surface* text_surface = TTF_RenderText_Solid(font, amount, (SDL_Color) { 0, 0, 0, 0 });
+            text = SDL_CreateTextureFromSurface(renderer, text_surface);
+            SDL_RenderCopy(renderer, text, NULL, &amount_rect);
+        }
+    }
+
+    //
+
+    SDL_SetRenderTarget(renderer, NULL);
+    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_Rect destination_rect = { -main_camera.x, -main_camera.y, MAP_WIDTH * TILE_SIZE * main_camera.zoom, MAP_HEIGHT * TILE_SIZE * main_camera.zoom };
+
+    SDL_RenderCopy(renderer, screen, NULL, &destination_rect);
+    SDL_RenderCopy(renderer, gui, NULL, NULL);
+    SDL_RenderPresent(renderer);
+}
+
 
 int main(void) {
     
@@ -34,9 +207,10 @@ int main(void) {
         spawn_entity(new_entity(entity_type_stone, 4 + i, 9));
         spawn_entity(new_entity(entity_type_stone, 4 + i, 10));
     }
-
-
     //
+
+    printf("Initializing!\n");
+
     if (TTF_Init() == -1)
     {
         printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
@@ -45,18 +219,18 @@ int main(void) {
     else if (!main_player) {
         printf("The main player is NULL!\n");
         return 1;
-    } 
+    }
     else if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
-    } 
+    }
     else if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
         printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
         return 1;
     }
     else {
         window = SDL_CreateWindow("CaveKingdom", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_BORDERLESS);
-        
+
         font = TTF_OpenFont("./assets/fonts/roboto.ttf", 36);
 
         if (font == NULL)
@@ -81,12 +255,12 @@ int main(void) {
             int d_key_pressed = 0;
 
             SDL_Event event;
-            while (quit == 0) { 
+            while (quit == 0) {
                 player_movement_x = 0;
                 player_movement_y = 0;
 
-                while (SDL_PollEvent(&event)) { 
-                    if (event.type == SDL_QUIT) quit = 1; 
+                while (SDL_PollEvent(&event)) {
+                    if (event.type == SDL_QUIT) quit = 1;
                     else if (event.type == SDL_KEYDOWN) {
                         if (SDLK_w == event.key.keysym.sym) w_key_pressed = 1;
                         else if (SDLK_s == event.key.keysym.sym) s_key_pressed = 1;
@@ -101,7 +275,7 @@ int main(void) {
 
                         //else if (SDLK_r == event.key.keysym.sym) print_inventory(&main_player->inventory);
 
-                        
+
                     }
                     else if (event.type == SDL_KEYUP) {
                         if (SDLK_w == event.key.keysym.sym) w_key_pressed = 0;
@@ -109,7 +283,7 @@ int main(void) {
                         else if (SDLK_a == event.key.keysym.sym) a_key_pressed = 0;
                         else if (SDLK_d == event.key.keysym.sym) d_key_pressed = 0;
                     }
-                } 
+                }
                 if (w_key_pressed) player_movement_y--;
                 if (s_key_pressed) player_movement_y++;
                 if (a_key_pressed) player_movement_x--;
